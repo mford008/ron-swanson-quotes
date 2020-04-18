d5fc96d455e0aeb2deff05fb48b5d50570f13f14
Maddy Ford <mrhf008@gmail.com>
2020-04-17 18:02:06 -0700

    updating Gif with hooks

--- src/components/Gif/Gif.js
+++ src/components/Gif/Gif.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Gif.css';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 
-class Gif extends Component {
-  render() {
-    return(
-      <div className="GifContainer">
-        <div className="Gif">
-            <img src={this.props.gif.images.fixed_width_downsampled.url}></img>
-        </div>
-        <div className="Gif__ButtonsContainer">
-          <CopyToClipboard className="Gif__ButtonContainer--copy" text={this.props.gif.images.fixed_width_downsampled.url}>
-            <button className="Gif__Button">Copy</button>
-          </CopyToClipboard>
-          <div className="Gif__ButtonContainer--favorite">
-            <button className="Gif__Button">Add to Favorites</button>
-          </div>
+const Gif = (props) => {
+  return(
+    <div className="GifContainer">
+      <div className="Gif">
+          <img src={props.gif.images.fixed_width_downsampled.url}></img>
+      </div>
+      <div className="Gif__ButtonsContainer">
+        <CopyToClipboard className="Gif__ButtonContainer--copy" text={props.gif.images.fixed_width_downsampled.url}>
+          <button className="Gif__Button">Copy</button>
+        </CopyToClipboard>
+        <div className="Gif__ButtonContainer--favorite">
+          <button className="Gif__Button">Add to Favorites</button>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Gif;
